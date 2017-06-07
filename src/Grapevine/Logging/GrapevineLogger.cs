4a92a7fb81cdd0e1dcc078a425f660ea0e741baa
Scott Offen <github@scottoffen.com>
2017-06-07 13:36:13 -0600

    Update logger abstract internal methods to public

--- src/Grapevine/Logging/GrapevineLogger.cs
+++ src/Grapevine/Logging/GrapevineLogger.cs
@@ -7,18 +7,18 @@ namespace Grapevine.Logging
         public abstract bool IsEnabled(GrapevineLogLevel level);
         public abstract void Log(GrapevineLogLevel level, string requestId, string msg, Exception exception = null);
 
-        internal void Trace(string msg, string requestId = null) { Log(GrapevineLogLevel.Trace, requestId, msg); }
-        internal void Debug(string msg, string requestId = null) { Log(GrapevineLogLevel.Debug, requestId, msg); }
-        internal void Info(string msg, string requestId = null) { Log(GrapevineLogLevel.Info, requestId, msg); }
-        internal void Warn(string msg, string requestId = null) { Log(GrapevineLogLevel.Warn, requestId, msg); }
-        internal void Error(string msg, string requestId = null) { Log(GrapevineLogLevel.Error, requestId, msg); }
-        internal void Fatal(string msg, string requestId = null) { Log(GrapevineLogLevel.Fatal, requestId, msg); }
+        public void Trace(string msg, string requestId = null) { Log(GrapevineLogLevel.Trace, requestId, msg); }
+        public void Debug(string msg, string requestId = null) { Log(GrapevineLogLevel.Debug, requestId, msg); }
+        public void Info(string msg, string requestId = null) { Log(GrapevineLogLevel.Info, requestId, msg); }
+        public void Warn(string msg, string requestId = null) { Log(GrapevineLogLevel.Warn, requestId, msg); }
+        public void Error(string msg, string requestId = null) { Log(GrapevineLogLevel.Error, requestId, msg); }
+        public void Fatal(string msg, string requestId = null) { Log(GrapevineLogLevel.Fatal, requestId, msg); }
 
-        internal void Trace(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Trace, requestId, msg, ex); }
-        internal void Debug(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Debug, requestId, msg, ex); }
-        internal void Info(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Info, requestId, msg, ex); }
-        internal void Warn(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Warn, requestId, msg, ex); }
-        internal void Error(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Error, requestId, msg, ex); }
-        internal void Fatal(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Fatal, requestId, msg, ex); }
+        public void Trace(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Trace, requestId, msg, ex); }
+        public void Debug(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Debug, requestId, msg, ex); }
+        public void Info(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Info, requestId, msg, ex); }
+        public void Warn(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Warn, requestId, msg, ex); }
+        public void Error(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Error, requestId, msg, ex); }
+        public void Fatal(string msg, Exception ex, string requestId = null) { Log(GrapevineLogLevel.Fatal, requestId, msg, ex); }
     }
 }
