2b1182491b6ecc04ff44db7d1e15863810a87650
Scott Offen <github@scottoffen.com>
2017-06-23 16:45:20 -0600

    Refactored IHttpRequest for properties needed in IRoute

--- src/Grapevine/Core/IHttpRequest.cs
+++ src/Grapevine/Core/IHttpRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Net;
 using Grapevine.Common;
 
@@ -6,37 +7,74 @@ namespace Grapevine.Core
 {
     public interface IHttpRequest
     {
+        /// <summary>
+        /// Gets the MIME type of the body data included in the request
+        /// </summary
         ContentType ContentType { get; }
 
+        /// <summary>
+        /// Gets the collection of header name/value pairs sent in the request
+        /// </summary>
         NameValueCollection Headers { get; }
 
+        /// <summary>
+        /// Gets the HTTPMethod specified by the client
+        /// </summary>
         HttpMethod HttpMethod { get; }
 
+        /// <summary>
+        /// A value that represents a unique identifier for this request
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Gets a representation of the HttpMethod and PathInfo of the request
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the URL information (without the host, port or query string) requested by the client
+        /// </summary>
         string PathInfo { get; }
 
+        /// <summary>
+        /// Gets or sets a dictionary of parameters provided in the PathInfo as identified by the processing route
+        /// </summary>
+        Dictionary<string, string> PathParameters { get; set; }
+
+        /// <summary>
+        /// Gets the query string included in the request
+        /// </summary>
         NameValueCollection QueryString { get; }
     }
 
     public class HttpRequest : IHttpRequest
     {
-        public HttpListenerRequest Advanced { get; protected internal set; }
+        private HttpListenerRequest _request;
 
         public ContentType ContentType { get; protected internal set; }
 
         public NameValueCollection Headers { get; protected internal set; }
 
         public HttpMethod HttpMethod { get; protected internal set; }
+
+        public string Id { get; protected internal set; }
+
+        public string Name { get; protected internal set; }
+
         public string PathInfo { get; protected internal set; }
+
+        public Dictionary<string, string> PathParameters { get; set; }
 
         public NameValueCollection QueryString { get; protected internal set; }
 
         protected internal HttpRequest(HttpListenerRequest request)
         {
-            Advanced = request;
+            _request = request;
 
-            PathInfo = Advanced.RawUrl.Split(new[] { '?' }, 2)[0];
-            ContentType = ContentTypes.FromString(Advanced.ContentType);
-            HttpMethod = HttpMethods.FromString(Advanced.HttpMethod);
+            PathInfo = _request.RawUrl.Split(new[] { '?' }, 2)[0];
+            ContentType = ContentTypes.FromString(_request.ContentType);
+            HttpMethod = HttpMethods.FromString(_request.HttpMethod);
         }
     }
 }
