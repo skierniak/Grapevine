79f96c7e74915035869b1ccb88edb76b3d702ee9
Scott Offen <github@scottoffen.com>
2017-06-26 15:06:23 -0600

    Added Json provider interfaces

---
+++ src/Grapevine/Core/Json/GrapevineJsonManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Grapevine.Core.Json
+{
+    public static class GrapevineJsonManager
+    {
+        static GrapevineJsonManager()
+        {
+            Provider = new JsonNotImplementedProvider();
+        }
+
+        /// <summary>
+        /// The logging provider used for logging in Grapevine.
+        /// </summary>
+        public static IGrapevineJsonProvider Provider { get; set; }
+    }
+}
---
+++ src/Grapevine/Core/Json/IGrapevineJsonProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace Grapevine.Core.Json
+{
+    public interface IGrapevineJsonProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        string Serialize<T>(T obj);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        T Deserialize<T>(string json);
+    }
+
+    public class JsonNotImplementedProvider : IGrapevineJsonProvider
+    {
+        public string Serialize<T>(T obj)
+        {
+            throw new System.NotImplementedException("An IGrapevineJsonProvider has not been registered");
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            throw new System.NotImplementedException("An IGrapevineJsonProvider has not been registered");
+        }
+    }
+}
