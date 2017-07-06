45a537c42e7ddd9c7e3fab1a08338ffc632e9dfc
Scott Offen <github@scottoffen.com>
2017-07-06 15:21:06 -0600

    Added Router

---
+++ src/Grapevine.Tests.Unit/Server/RouterFacts.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Grapevine.Common;
+using Grapevine.Core;
+using Grapevine.Server;
+using Grapevine.Tests.Sample;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Grapevine.Tests.Unit.Server
+{
+    public class RouterFacts
+    {
+        public class Import
+        {
+            private readonly Router _router = new Router();
+
+            [Fact]
+            public void AppendsRoutesFromImportedRouter()
+            {
+                var route1 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route2 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route3 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route4 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+
+                var router = new Router();
+                router.Register(route1);
+                router.Register(route2);
+                router.Register(route3);
+
+                _router.Register(route4);
+                _router.Import(router);
+
+                _router.RegisteredRoutes.ShouldSatisfyAllConditions
+                (
+                    () => _router.RegisteredRoutes.Count.ShouldBe(4),
+                    () => _router.RegisteredRoutes[0].ShouldBe(route4),
+                    () => _router.RegisteredRoutes[1].ShouldBe(route1),
+                    () => _router.RegisteredRoutes[2].ShouldBe(route2),
+                    () => _router.RegisteredRoutes[3].ShouldBe(route3)
+                );
+            }
+
+            [Fact]
+            public void AppendsRoutesFromImportedRouterWithoutDuplication()
+            {
+                var route1 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route2 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route3 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+                var route4 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+
+                var router = new Router();
+                router.Register(route1);
+                router.Register(route2);
+                router.Register(route3);
+                router.Register(route4);
+
+                _router.Register(route4);
+                _router.Import(router);
+
+                _router.RegisteredRoutes.ShouldSatisfyAllConditions
+                (
+                    () => _router.RegisteredRoutes.Count.ShouldBe(4),
+                    () => _router.RegisteredRoutes[0].ShouldBe(route4),
+                    () => _router.RegisteredRoutes[1].ShouldBe(route1),
+                    () => _router.RegisteredRoutes[2].ShouldBe(route2),
+                    () => _router.RegisteredRoutes[3].ShouldBe(route3)
+                );
+            }
+        }
+
+        public class InsertAt
+        {
+            private readonly Router _router = new Router();
+
+            private readonly Grapevine.Server.Route _route1 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route2 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route3 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route4 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+
+            [Theory]
+            [InlineData(-1, false)]
+            [InlineData(0, true)]
+            [InlineData(1, true)]
+            [InlineData(2, true)]
+            [InlineData(3, true)]
+            [InlineData(4, false)]
+            public void InsertsRouteAtIndexWhenIndexIsInRange(int position, bool shouldInsert)
+            {
+                _router.Register(_route1);
+                _router.Register(_route2);
+                _router.Register(_route3);
+
+                var count = shouldInsert ? 4 : 3;
+                var result = shouldInsert ? position + 1 : position;
+
+                var idx = _router.InsertAt(position, _route4);
+
+                idx.ShouldBe(result);
+                _router.RegisteredRoutes.Count.ShouldBe(count);
+
+                if (shouldInsert)
+                {
+                    _router.RegisteredRoutes[position].ShouldBe(_route4);
+                }
+                else
+                {
+                    _router.RegisteredRoutes.Contains(_route4).ShouldBeFalse();
+                }
+            }
+
+            [Fact]
+            public void DoesNotInsertDuplicateRoute()
+            {
+                _router.Register(_route1);
+                _router.Register(_route2);
+                _router.Register(_route3);
+                _router.Register(_route4);
+                var position = 2;
+
+                var idx = _router.InsertAt(position, _route4);
+
+                _router.RegisteredRoutes.ShouldSatisfyAllConditions
+                (
+                    () => _router.RegisteredRoutes.Count.ShouldBe(4),
+                    () => _router.RegisteredRoutes[0].ShouldBe(_route1),
+                    () => _router.RegisteredRoutes[1].ShouldBe(_route2),
+                    () => _router.RegisteredRoutes[2].ShouldBe(_route3),
+                    () => _router.RegisteredRoutes[3].ShouldBe(_route4)
+                );
+
+                idx.ShouldBe(position);
+            }
+        }
+
+        public class Register
+        {
+            private readonly Router _router = new Router();
+
+            private readonly Grapevine.Server.Route _route1 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route2 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route3 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+            private readonly Grapevine.Server.Route _route4 = new Grapevine.Server.Route(context => { }, HttpMethod.GET, "");
+
+            [Fact]
+            public void RegistersRouteToEndOfRoutingTable()
+            {
+                _router.Register(_route1);
+                _router.Register(_route2);
+                _router.Register(_route3);
+                _router.Register(_route4);
+
+                _router.RegisteredRoutes.ShouldSatisfyAllConditions
+                (
+                    () => _router.RegisteredRoutes.Count.ShouldBe(4),
+                    () => _router.RegisteredRoutes[0].ShouldBe(_route1),
+                    () => _router.RegisteredRoutes[1].ShouldBe(_route2),
+                    () => _router.RegisteredRoutes[2].ShouldBe(_route3),
+                    () => _router.RegisteredRoutes[3].ShouldBe(_route4)
+                );
+            }
+
+            [Fact]
+            public void DoesNotRegisterDuplicateRoute()
+            {
+                _router.Register(_route1);
+                _router.Register(_route2);
+                _router.Register(_route3);
+                _router.Register(_route4);
+
+                _router.Register(_route1);
+                _router.Register(_route2);
+                _router.Register(_route3);
+                _router.Register(_route4);
+
+                _router.RegisteredRoutes.ShouldSatisfyAllConditions
+                (
+                    () => _router.RegisteredRoutes.Count.ShouldBe(4),
+                    () => _router.RegisteredRoutes[0].ShouldBe(_route1),
+                    () => _router.RegisteredRoutes[1].ShouldBe(_route2),
+                    () => _router.RegisteredRoutes[2].ShouldBe(_route3),
+                    () => _router.RegisteredRoutes[3].ShouldBe(_route4)
+                );
+            }
+        }
+
+        public class Scan
+        {
+            private readonly Type _type = typeof(TypeScannerA);
+            private readonly Assembly _assembly = AppDomain.CurrentDomain.GetAssemblies().First(x => x.GetName().Name == "Grapevine.Tests.Sample");
+
+            private readonly Router _router = new Router();
+
+            [Fact]
+            public void AppendsRoutesToRoutingTable()
+            {
+                _router.Scan();
+                _router.RegisteredRoutes.Count.ShouldBe(3);
+            }
+
+            [Fact]
+            public void DoesNotAppendDuplicateRoutes()
+            {
+                var route = new Grapevine.Server.Route(_type.GetMethod("RouteA"), HttpMethod.ALL, "");
+                _router.Register(route);
+
+                _router.Scan();
+
+                _router.RegisteredRoutes[0].Name.ShouldBe("Grapevine.Tests.Sample.TypeScannerA.RouteA");
+                _router.RegisteredRoutes[1].Name.ShouldBe("Grapevine.Tests.Sample.TypeScannerB.RouteA");
+                _router.RegisteredRoutes[2].Name.ShouldBe("Grapevine.Tests.Sample.TypeScannerC.RouteA");
+
+                _router.RegisteredRoutes.Count.ShouldBe(3);
+            }
+        }
+
+        public class Route
+        {
+        }
+
+        public class RoutesFor
+        {
+            private readonly Router _router = new Router();
+            private readonly IRoute _route = Substitute.For<IRoute>();
+
+            [Fact]
+            private void ReturnsEmptyListWhenNoMatchingRoutesFound()
+            {
+                _router.RoutesFor(Substitute.For<IHttpContext>()).Count.ShouldBe(0);
+
+                _route.Matches(Arg.Any<IHttpContext>()).Returns(false);
+                _route.Enabled.Returns(true);
+
+                _router.Register(_route);
+
+                _router.RoutesFor(Substitute.For<IHttpContext>()).Count.ShouldBe(0);
+            }
+
+            [Fact]
+            private void ReturnsOnlyEnabledMatchingRoutes()
+            {
+                _route.Matches(Arg.Any<IHttpContext>()).Returns(true);
+                _route.Enabled.Returns(true);
+
+                _router.Register(_route);
+
+                var list = _router.RoutesFor(Substitute.For<IHttpContext>());
+
+                list.Count.ShouldBe(1);
+                list[0].ShouldBe(_route);
+            }
+
+            [Fact]
+            private void DoesNotReturnsDisabledMatchingRoutes()
+            {
+                _route.Matches(Arg.Any<IHttpContext>()).Returns(true);
+                _route.Enabled.Returns(false);
+
+                _router.Register(_route);
+
+                _router.RoutesFor(Substitute.For<IHttpContext>()).Count.ShouldBe(0);
+            }
+        }
+
+        public class OnBeforeRouting
+        {
+            private readonly Router _router = new Router();
+
+            [Fact]
+            public void DoesNotThrowExceptionWhenNoEventsHaveBeenAdded()
+            {
+                _router.OnBeforeRouting(Substitute.For<IHttpContext>());
+            }
+
+            [Fact]
+            public void InvokesEventsInOrderAdded()
+            {
+                var results = new List<string>();
+
+                _router.BeforeRouting += context => { results.Add("First"); };
+                _router.BeforeRouting += context => { results.Add("Second"); };
+                _router.BeforeRouting += context => { results.Add("Third"); };
+
+                _router.OnBeforeRouting(Substitute.For<IHttpContext>());
+
+                results.ShouldSatisfyAllConditions
+                (
+                    () => results.Count.ShouldBe(3),
+                    () => results[0].ShouldBe("First"),
+                    () => results[1].ShouldBe("Second"),
+                    () => results[2].ShouldBe("Third")
+                );
+            }
+        }
+
+        public class OnAfterRouting
+        {
+            private readonly Router _router = new Router();
+
+            [Fact]
+            public void DoesNotThrowExceptionWhenNoEventsHaveBeenAdded()
+            {
+                _router.OnAfterRouting(Substitute.For<IHttpContext>());
+            }
+
+            [Fact]
+            public void InvokesEventsInReverseOrderAdded()
+            {
+                var results = new List<string>();
+
+                _router.AfterRouting += context => { results.Add("First"); };
+                _router.AfterRouting += context => { results.Add("Second"); };
+                _router.AfterRouting += context => { results.Add("Third"); };
+
+                _router.OnAfterRouting(Substitute.For<IHttpContext>());
+
+                results.ShouldSatisfyAllConditions
+                (
+                    () => results.Count.ShouldBe(3),
+                    () => results[0].ShouldBe("Third"),
+                    () => results[1].ShouldBe("Second"),
+                    () => results[2].ShouldBe("First")
+                );
+            }
+        }
+    }
+}
--- src/Grapevine/Server/Router.cs
+++ src/Grapevine/Server/Router.cs
@@ -1,6 +1,260 @@
-﻿namespace Grapevine.Server
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Grapevine.Core;
+using Grapevine.Core.Exceptions;
+using Grapevine.Core.Logging;
+
+namespace Grapevine.Server
 {
-    public class Router
+    /// <summary>
+    /// Delegate for the <see cref="IRouter.BeforeRouting"/> and <see cref="IRouter.AfterRouting"/> events
+    /// </summary>
+    /// <param name="context">The <see cref="IHttpContext"/> that is being routed.</param>
+    public delegate void RoutingEventHandler(IHttpContext context);
+
+    /// <summary>
+    /// Provides a mechanism to register routes and invoke them according to the produced routing table
+    /// </summary>
+    public interface IRouter
     {
+        /// <summary>
+        /// Raised after a request has completed invoking matching routes
+        /// </summary>
+        event RoutingEventHandler AfterRouting;
+
+        /// <summary>
+        /// Raised prior to sending any request though matching routes
+        /// </summary>
+        event RoutingEventHandler BeforeRouting;
+
+        /// <summary>
+        /// Scan the assembly for routes based on inclusion and exclusion rules
+        /// </summary>
+        IRouteScanner Scanner { get; set; }
+
+        /// <summary>
+        /// Gets a list of registered routes in the order they were registered
+        /// </summary>
+        IList<IRoute> RoutingTable { get; }
+
+        /// <summary>
+        /// Adds the routes in router parameter to the end of the current routing table
+        /// </summary>
+        /// <param name="router"></param>
+        /// <returns>IRouter</returns>
+        IRouter Import(IRouter router);
+
+        /// <summary>
+        /// Inserts the routes into the routing table at the specified index
+        /// </summary>
+        int InsertAt(int index, IRoute route);
+
+        /// <summary>
+        /// Adds the route to the routing table
+        /// </summary>
+        /// <param name="route"></param>
+        /// <returns>IRouter</returns>
+        IRouter Register(IRoute route);
+
+        /// <summary>
+        /// Adds all RestRoutes returned from RouteScanner.Scan() to the routing table
+        /// </summary>
+        /// <returns>IRouter</returns>
+        IRouter Scan();
+
+        /// <summary>
+        /// Routes the IHttpContext through all enabled registered routes that match the IHttpConext provided
+        /// </summary>
+        /// <param name="state"></param>
+        void Route(object state);
+
+        /// <summary>
+        /// Routes the IHttpContext through the list of routes provided
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="routing"></param>
+        void Route(IHttpContext context, IList<IRoute> routing);
+
+        /// <summary>
+        /// Gets a list of enabled registered routes that match the IHttpContext provided
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>IList&lt;IRoute&gt;</returns>
+        IList<IRoute> RoutesFor(IHttpContext context);
+    }
+
+    public class Router : IRouter
+    {
+        protected internal readonly IList<IRoute> RegisteredRoutes = new List<IRoute>();
+
+        public event RoutingEventHandler AfterRouting;
+        public event RoutingEventHandler BeforeRouting;
+
+        public IRouteScanner Scanner { get; set; } = new RouteScanner();
+        public IList<IRoute> RoutingTable => RegisteredRoutes.ToList().AsReadOnly();
+
+        protected internal readonly GrapevineLogger Logger = GrapevineLogManager.GetCurrentClassLogger();
+
+        public IRouter Import(IRouter router)
+        {
+            AppendRoutingTable(router.RoutingTable);
+            return this;
+        }
+
+        public int InsertAt(int index, IRoute route)
+        {
+            if (index < 0 || index > RegisteredRoutes.Count || RegisteredRoutes.Any(r => r.Name == route.Name)) return index;
+            RegisteredRoutes.Insert(index, route);
+            return index + 1;
+        }
+
+        public IRouter Register(IRoute route)
+        {
+            AppendRoutingTable(route);
+            return this;
+        }
+
+        public IRouter Scan()
+        {
+            AppendRoutingTable(Scanner.Scan());
+            return this;
+        }
+
+        public void Route(object state)
+        {
+            var context = state as IHttpContext;
+            if (context == null) return;
+
+            try
+            {
+                //    if (context.Request.HttpMethod == HttpMethod.GET)
+                //    {
+                //        foreach (var folder in context.Server.PublicFolders)
+                //        {
+                //            folder.SendFile(context);
+                //            if (context.WasRespondedTo) return;
+                //        }
+                //    }
+
+                //    Route(context, RoutesFor(context));
+            }
+            catch (Exception e)
+            {
+                //    Logger.Log(new LogEvent { Exception = e, Level = LogLevel.Error, Message = e.Message });
+                //    if (context.Server.EnableThrowingExceptions) throw;
+
+                //    if (context.WasRespondedTo) return;
+
+                //    if (e is NotFoundException)
+                //    {
+                //        context.Response.SendResponse(HttpStatusCode.NotFound, e.Message);
+                //        return;
+                //    }
+
+                //    if (e is NotImplementedException)
+                //    {
+                //        context.Response.SendResponse(HttpStatusCode.NotImplemented, e.Message);
+                //        return;
+                //    }
+
+                //    context.Response.SendResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public void Route(IHttpContext context, IList<IRoute> routing)
+        {
+            if (routing == null || !routing.Any()) throw new RouteNotFoundException(context.Request.HttpMethod, context.Request.PathInfo);
+
+            var total = routing.Count;
+            var counter = 0;
+
+            Logger.Trace($"{context.Request.Name} has {total} matching routes", context.Request.Id);
+
+            try
+            {
+                OnBeforeRouting(context);
+
+                foreach (var route in routing.Where(route => route.Enabled))
+                {
+                    counter++;
+                    route.Invoke(context);
+
+                    Logger.Trace($"{counter}/{total} {route.Name}", context.Request.Id);
+
+                    if (context.WasRespondedTo) break;
+                }
+            }
+            catch (HttpListenerException e)
+            {
+                var msg = e.NativeErrorCode == 64
+                    ? "Connection aborted by client"
+                    : "An error occured while attempting to respond to the request";
+                Logger.Warn(msg, e, context.Request.Id);
+            }
+            finally
+            {
+                OnAfterRouting(context);
+                Logger.Trace($"{counter} of {total} routes invoked", context.Request.Id);
+            }
+
+            if (!context.WasRespondedTo)
+                throw new RouteNotFoundException(context.Request.HttpMethod, context.Request.PathInfo);
+        }
+
+        public IList<IRoute> RoutesFor(IHttpContext context)
+        {
+            return RegisteredRoutes.Where(r => r.Matches(context) && r.Enabled).ToList();
+        }
+
+        /// <summary>
+        /// Event handler for when the <see cref="BeforeRouting"/> event is raised
+        /// </summary>
+        /// <param name="context">The <see cref="IHttpContext"/> being routed</param>
+        protected internal void OnBeforeRouting(IHttpContext context)
+        {
+            BeforeRouting?.Invoke(context);
+        }
+
+        /// <summary>
+        /// Event handler for when the <see cref="AfterRouting"/> event is raised
+        /// </summary>
+        /// <param name="context">The <see cref="IHttpContext"/> being routed</param>
+        protected internal void OnAfterRouting(IHttpContext context)
+        {
+            if (AfterRouting == null) return;
+            foreach (var action in AfterRouting.GetInvocationList().Reverse().Cast<RoutingEventHandler>())
+            {
+                action(context);
+            }
+        }
+
+        /// <summary>
+        /// Adds the route to the routing table excluding duplicates
+        /// </summary>
+        /// <param name="route"></param>
+        protected internal void AppendRoutingTable(IRoute route)
+        {
+            if (RegisteredRoutes.All(r => r.Name != route.Name)) RegisteredRoutes.Add(route);
+        }
+
+        /// <summary>
+        /// Adds the routes to the routing table excluding duplicates
+        /// </summary>
+        /// <param name="routes"></param>
+        protected internal void AppendRoutingTable(IEnumerable<IRoute> routes)
+        {
+            routes.ToList().ForEach(AppendRoutingTable);
+        }
+    }
+
+    public static class IRouterExtensions
+    {
+        /* Create extensions for:
+         * Import
+         * Insert
+         * Register
+         */
     }
 }
