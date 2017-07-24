4da58e46483ab91b4b8c78e4ed841fd79d3aaaec
Scott Offen <github@scottoffen.com>
2017-07-24 16:51:23 -0600

    Added RestCluster and unit tests

---
+++ src/Grapevine.Tests.Unit/Server/RestClusterFacts.cs
@@ -0,0 +1,297 @@
+﻿using System.Collections.Generic;
+using Grapevine.Core;
+using Grapevine.Server;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Grapevine.Tests.Unit.Server
+{
+    public class RestClusterFacts
+    {
+        protected const string GlobalPrefix = "GlobalPrefix";
+        protected IRestServer Server = Substitute.For<IRestServer>();
+        protected RestCluster Cluster = new RestCluster();
+        protected List<Handlers> Raised = new List<Handlers>();
+
+        public RestClusterFacts()
+        {
+            Cluster.BeforeStartingAll += () => { Raised.Add(Handlers.BeforeStartAll); };
+            Cluster.BeforeStartingEach += _ => { Raised.Add(Handlers.BeforeStartEach); };
+            Cluster.AfterStartingEach += _ => { Raised.Add(Handlers.AfterStartEach); };
+            Cluster.AfterStartingAll += () => { Raised.Add(Handlers.AfterStartAll); };
+
+            Cluster.BeforeStoppingAll += () => { Raised.Add(Handlers.BeforeStopAll); };
+            Cluster.BeforeStoppingEach += _ => { Raised.Add(Handlers.BeforeStopEach); };
+            Cluster.AfterStoppingEach += _ => { Raised.Add(Handlers.AfterStopEach); };
+            Cluster.AfterStoppingAll += () => { Raised.Add(Handlers.AfterStopAll); };
+
+            Server.When(x => x.Start()).Do(_ => { Raised.Add(Handlers.ServerStart); });
+            Server.When(x => x.Stop()).Do(_ => { Raised.Add(Handlers.ServerStop); });
+            Server.ListenerPrefix.Returns(GlobalPrefix);
+        }
+
+        [Fact]
+        public void AddGetRemoveServers()
+        {
+            var a = new StubServer("servera");
+            var b = new StubServer("serverb");
+            var c = new StubServer("serverc");
+            var d = new StubServer("serverd");
+
+            var cluster = new RestCluster();
+            cluster.ShouldNotBeNull();
+            cluster.Count.ShouldBe(0);
+
+            cluster.Add("servera", a);
+            cluster.Add("serverb", b);
+
+            cluster.Count.ShouldBe(2);
+            cluster.Get("servera").ShouldBe(a);
+            cluster.Get("serverb").ShouldBe(b);
+
+            cluster["serverc"] = c;
+            cluster.Count.ShouldBe(3);
+            cluster.Get("serverc").ShouldBe(c);
+
+            cluster.Remove("serverd").ShouldBeTrue();
+            cluster.Count.ShouldBe(3);
+
+            cluster.Remove("servera").ShouldBeTrue();
+            cluster.Count.ShouldBe(2);
+
+            cluster.Add(d);
+            cluster.Count.ShouldBe(3);
+            cluster[d.ListenerPrefix].ShouldBe(d);
+            cluster.Get(d.ListenerPrefix).ShouldBe(d);
+        }
+
+        [Fact]
+        public void GetReturnsNullWhenItDoesNotExist()
+        {
+            Cluster.Add(Server);
+
+            Cluster.Get(GlobalPrefix).ShouldBe(Server);
+            Cluster.Get("NotAvailable").ShouldBeNull();
+        }
+
+        [Fact]
+        public void ServersAddAfterClusterStartsShouldBeStarted()
+        {
+            var expected = new List<Handlers> {Handlers.BeforeStartEach, Handlers.ServerStart, Handlers.AfterStartEach};
+
+            Cluster.Started = true;
+            Cluster.Add(Server);
+
+            Server.Received().Start();
+            Raised.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void ServersAddBeforeClusterStartsShouldNotBeStarted()
+        {
+            Cluster.Add(Server);
+
+            Server.DidNotReceive().Start();
+            Raised.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void ServersRemovedAfterStartedShouldBeStopped()
+        {
+            var expected = new List<Handlers> {Handlers.BeforeStopEach, Handlers.ServerStop, Handlers.AfterStopEach};
+
+            Cluster.Add(Server);
+            Cluster.Started = true;
+
+            Cluster.Remove(Server.ListenerPrefix);
+
+            Server.Received().Stop();
+            Raised.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void ServersRemovedBeforeStartedShouldNotBeStopped()
+        {
+            Cluster.Add(Server);
+
+            Cluster.Remove(Server.ListenerPrefix);
+
+            Server.DidNotReceive().Stop();
+            Raised.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void StartAll()
+        {
+            var expected = new List<Handlers>
+            {
+                Handlers.BeforeStartAll,
+                Handlers.BeforeStartEach,
+                Handlers.AfterStartEach,
+                Handlers.BeforeStartEach,
+                Handlers.AfterStartEach,
+                Handlers.BeforeStartEach,
+                Handlers.AfterStartEach,
+                Handlers.AfterStartAll
+            };
+
+            var a = new StubServer("servera");
+            var b = new StubServer("serverb");
+            var c = new StubServer("serverc");
+            var d = new StubServer("serverd") {IsListening = true};
+
+            Cluster.Add(a);
+            Cluster.Add(b);
+            Cluster.Add(c);
+            Cluster.Add(d);
+
+            Cluster.StartAll();
+
+            a.IsListening.ShouldBeTrue();
+            b.IsListening.ShouldBeTrue();
+            c.IsListening.ShouldBeTrue();
+            d.IsListening.ShouldBeTrue();
+
+            Raised.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void StartAllDoesNotRunIfStartedIsTrue()
+        {
+            Cluster.Add(Server);
+            Cluster.Started = true;
+
+            Cluster.StartAll();
+
+            Server.DidNotReceive().Start();
+            Raised.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void StopAll()
+        {
+            var expected = new List<Handlers>
+            {
+                Handlers.BeforeStopAll,
+                Handlers.BeforeStopEach,
+                Handlers.AfterStopEach,
+                Handlers.BeforeStopEach,
+                Handlers.AfterStopEach,
+                Handlers.BeforeStopEach,
+                Handlers.AfterStopEach,
+                Handlers.AfterStopAll
+            };
+
+            var a = new StubServer("servera");
+            var b = new StubServer("serverb") { IsListening = true };
+            var c = new StubServer("serverc") { IsListening = true };
+            var d = new StubServer("serverd") { IsListening = true };
+
+            Cluster.Add(a);
+            Cluster.Add(b);
+            Cluster.Add(c);
+            Cluster.Add(d);
+
+            Cluster.Started = true;
+            Cluster.StopAll();
+
+            a.IsListening.ShouldBeFalse();
+            b.IsListening.ShouldBeFalse();
+            c.IsListening.ShouldBeFalse();
+            d.IsListening.ShouldBeFalse();
+
+            Raised.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void StopAllDoesNotRunIfStartedIsFalse()
+        {
+            Cluster.Add(Server);
+
+            Cluster.StopAll();
+
+            Server.DidNotReceive().Stop();
+            Raised.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public void StartAndStopDoesNotErrorWhenNoHandlersAreDefined()
+        {
+            var servera = new StubServer("servera");
+            var serverb = new StubServer("serverb");
+            var serverc = new StubServer("serverc");
+            var serverd = new StubServer("serverd");
+
+            var cluster = new RestCluster();
+            cluster.Add(servera);
+            cluster.Add(serverb);
+            cluster.Add(serverc);
+            cluster.Add(serverd);
+
+            cluster.StartAll();
+
+            servera.IsListening.ShouldBeTrue();
+            serverb.IsListening.ShouldBeTrue();
+            serverc.IsListening.ShouldBeTrue();
+            serverd.IsListening.ShouldBeTrue();
+
+            cluster.StopAll();
+
+            servera.IsListening.ShouldBeFalse();
+            serverb.IsListening.ShouldBeFalse();
+            serverc.IsListening.ShouldBeFalse();
+            serverd.IsListening.ShouldBeFalse();
+        }
+    }
+
+    public enum Handlers
+    {
+        BeforeStartAll,
+        BeforeStartEach,
+        AfterStartEach,
+        AfterStartAll,
+        BeforeStopAll,
+        BeforeStopEach,
+        AfterStopEach,
+        AfterStopAll,
+        ServerStart,
+        ServerStop
+    }
+
+    public class StubServer : IRestServer
+    {
+        public IDictionary<string, object> Properties { get; }
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event ServerEventHandler AfterStarting;
+        public event ServerEventHandler AfterStopping;
+        public event ServerEventHandler BeforeStarting;
+        public event ServerEventHandler BeforeStopping;
+        public IList<IContentFolder> ContentFolders { get; }
+        public string Host { get; set; }
+        public bool IsListening { get; set; }
+        public IHttpListener Listener { get; }
+        public string ListenerPrefix { get; }
+        public string Port { get; set; }
+        public IRouter Router { get; set; }
+        public bool UseHttps { get; set; }
+        public void Start()
+        {
+            IsListening = true;
+        }
+
+        public void Stop()
+        {
+            IsListening = false;
+        }
+
+        public StubServer(string prefix)
+        {
+            ListenerPrefix = prefix;
+        }
+    }
+}
--- src/Grapevine/Server/RestCluster.cs
+++ src/Grapevine/Server/RestCluster.cs
@@ -1,6 +1,163 @@
-﻿namespace Grapevine.Server
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Grapevine.Server
 {
+    public delegate void ClusterEventHandler();
+
+    /// <summary>
+    /// Container class to store and manage starting and stopping multiple RestServer objects
+    /// </summary>
     public class RestCluster
     {
+        protected internal readonly Dictionary<string, IRestServer> Servers = new Dictionary<string, IRestServer>();
+        protected internal bool Started;
+
+        /// <summary>
+        /// Gets the number of servers in the cluster
+        /// </summary>
+        public int Count => Servers.Count;
+
+        /// <summary>
+        /// Raised before the cluster starts all servers in the cluster.
+        /// </summary>
+        public event ClusterEventHandler BeforeStartingAll;
+
+        /// <summary>
+        /// Raised after the cluster starts all servers in the cluster.
+        /// </summary>
+        public event ClusterEventHandler AfterStartingAll;
+
+        /// <summary>
+        /// Raised before the cluster stops all servers in the cluster.
+        /// </summary>
+        public event ClusterEventHandler BeforeStoppingAll;
+
+        /// <summary>
+        /// Raised after the cluster stops all servers in the cluster.
+        /// </summary>
+        public event ClusterEventHandler AfterStoppingAll;
+
+        /// <summary>
+        /// Raised before starting each server in the cluster.
+        /// </summary>
+        public event ServerEventHandler BeforeStartingEach;
+
+        /// <summary>
+        /// Raised after starting each server in the cluster.
+        /// </summary>
+        public event ServerEventHandler AfterStartingEach;
+
+        /// <summary>
+        /// Raised before stopping each server in the cluster.
+        /// </summary>
+        public event ServerEventHandler BeforeStoppingEach;
+
+        /// <summary>
+        /// Raised after stopping each server in the cluster.
+        /// </summary>
+        public event ServerEventHandler AfterStoppingEach;
+
+        /// <summary>
+        /// Convenience indexer; synonym for Add and Get methods
+        /// </summary>
+        /// <param name="label"></param>
+        /// <returns>Get or set the instance of IRestServer assigned to the label</returns>
+        public IRestServer this[string label]
+        {
+            get { return Get(label); }
+            set { Add(label, value); }
+        }
+
+        /// <summary>
+        /// Adds a server to the cluster
+        /// </summary>
+        /// <param name="server"></param>
+        /// <returns>Assigned labled of the server that was addded</returns>
+        public string Add(IRestServer server)
+        {
+            Add(server.ListenerPrefix, server);
+            return server.ListenerPrefix;
+        }
+
+        /// <summary>
+        /// Adds a server to the cluster
+        /// </summary>
+        public void Add(string label, IRestServer server)
+        {
+            Servers.Add(label, server);
+            if (Started) Start(server);
+        }
+
+        /// <summary>
+        /// Retrieve an IRestServer from the cluster
+        /// </summary>
+        public IRestServer Get(string label)
+        {
+            return Servers.ContainsKey(label) ? Servers[label] : null;
+        }
+
+        /// <summary>
+        /// Stop and remove a server from the cluster
+        /// </summary>
+        public bool Remove(string label)
+        {
+            if (!Servers.ContainsKey(label)) return true;
+
+            var server = Get(label);
+            if (Started) Stop(server);
+
+            return Servers.Remove(label);
+        }
+
+        /// <summary>
+        /// Starts each server in the cluster
+        /// </summary>
+        public void StartAll()
+        {
+            if (Started) return;
+
+            BeforeStartingAll?.Invoke();
+
+            foreach (var server in Servers.Values.Where(server => !server.IsListening))
+            {
+                Start(server);
+            }
+
+            AfterStartingAll?.Invoke();
+            Started = true;
+        }
+
+        /// <summary>
+        /// Stops each server in the cluster
+        /// </summary>
+        public void StopAll()
+        {
+            if (!Started) return;
+
+            BeforeStoppingAll?.Invoke();
+
+            foreach (var server in Servers.Values.Where(_ => _.IsListening))
+            {
+                Stop(server);
+            }
+
+            AfterStoppingAll?.Invoke();
+            Started = false;
+        }
+
+        protected internal void Start(IRestServer server)
+        {
+            BeforeStartingEach?.Invoke(server);
+            server.Start();
+            AfterStartingEach?.Invoke(server);
+        }
+
+        protected internal void Stop(IRestServer server)
+        {
+            BeforeStoppingEach?.Invoke(server);
+            server.Stop();
+            AfterStoppingEach?.Invoke(server);
+        }
     }
 }
